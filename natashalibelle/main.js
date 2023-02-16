0b2514572619d5df1300010fab25bb21f0cddb27
ArtakSaakyan <artak.saakyan.99@mail.ru>
2023-02-16 11:04:20 +0500

    Готовый слайдер Owl с фильтрацией

--- natashalibelle/main.js
+++ natashalibelle/main.js
@@ -1,6 +1,7 @@
-jQuery(document).ready(function ($) {
-	let owl = $(".owl-carousel");
-	owl.owlCarousel({
+let owl = $(".owl-carousel")
+
+function owlStone() {
+  owl.owlCarousel({
     nav: true,
     navText: [`<button type="button" role="presentation" class="owl-prev">
                   <i class="svg left colored_theme_hover_text">
@@ -16,18 +17,122 @@ jQuery(document).ready(function ($) {
                     </svg>
                   </i>
                 </button>`],
-		loop: true,
-    margin: -2,
-		responsive: {
-			0: {
-				items: 2
-			},
-			768: {
-				items: 3,
-			},
-			992: {
-				items: 4,
-			}
-		}
-	})
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      }
+    }
+  })
+}
+
+$(function() {
+  owlStone()
+})
+
+const dataSliderId = document.querySelectorAll('[data-slider]')
+
+let sliderId = 0
+dataSliderId.forEach(item => {
+  item.setAttribute('data-slider-id', sliderId)
+  sliderId++
+})
+
+const dataStoneList = document.querySelectorAll('.stone-list')
+const stonesItemList = []
+dataStoneList.forEach(item => {
+  stonesItemList.push(item.querySelectorAll('.stone-item'))
+})
+
+let categoryList = []
+for (let i = 0; i < dataStoneList.length; i++) {
+  categoryList.push([])
+}
+
+function checkedCategory(evt) {
+  let event = evt[0]
+  let categoryId = event.getAttribute('data-category-id')
+  let dataSliderId = evt.parent().parent().parent().parent().parent().find('[data-slider-id]').attr('data-slider-id')[0]
+  
+  if (categoryList[dataSliderId].includes(Number(categoryId))) {
+    let index = categoryList[dataSliderId].indexOf(Number(categoryId));
+    if (index !== -1) categoryList[dataSliderId].splice(index, 1);
+  } else if (categoryId === 'all') {
+    categoryList[dataSliderId].length = 0
+  } else {
+    categoryList[dataSliderId].push(Number(categoryId))
+  }
+}
+
+function updateProductList(dataSliderId, stonesList, stonesItemList) {
+  owl.trigger('destroy.owl.carousel')
+  stonesList.innerHTML = ''
+  let countItem = 0
+  
+  stonesItemList.forEach(item => {
+    let itemProductID = Number(item.getAttribute('data-category-id'))
+    if (categoryList[dataSliderId].length === 0) {
+      stonesList.appendChild(item)
+      countItem += 1
+    } else if (categoryList[dataSliderId].includes(itemProductID) === true) {
+      stonesList.appendChild(item)
+      countItem += 1
+    }
+  })
+
+  $(function() {
+    owlStone()
+  })
+}
+
+let checkedList = []
+for (let i = 0; i < dataStoneList.length; i++) {
+  checkedList.push([])
+}
+
+$('[data-category-list]').on('click', '[data-click="category"]', function() {
+  let dataSliderId = $(this).parent().parent().parent().parent().parent().find('[data-slider-id]').attr('data-slider-id')[0]
+  $(this).toggleClass('section-detail-list__item--active')
+  $(this).toggleClass('colored_theme_bg')
+  $(this).parent().parent().find('[data-category-id="all"]').removeClass('section-detail-list__item--active')
+  $(this).parent().parent().find('[data-category-id="all"]').removeClass('colored_theme_bg')
+  updateProductList(dataSliderId, dataStoneList[dataSliderId], stonesItemList[dataSliderId])
+
+  let checked = $(this).attr('data-checked')
+
+  if (checked !== 'true') {
+    $(this).attr('data-checked', true)
+    checkedList[dataSliderId].push(checked)
+  } else {
+    $(this).attr('data-checked', false)
+    checkedList[dataSliderId].pop()
+  }
+
+  if (checkedList[dataSliderId].length === 0) {
+    $(this).parent().parent().find('[data-checked-all]').attr('data-checked-all', true)
+    $(this).parent().parent().find('[data-checked-all]').addClass('section-detail-list__item--active')
+    $(this).parent().parent().find('[data-checked-all]').addClass('colored_theme_bg')
+  } else {
+    $(this).parent().parent().find('[data-checked-all]').attr('data-checked-all', false)
+  }
+})
+
+$('[data-category-list]').on('click', '[data-click="all"]', function() {
+  let checkedAll = $(this).parent().parent().find('[data-checked-all]').attr('data-checked-all')
+  if (checkedAll !== 'true') {
+    $(this).parent().parent().find('[data-checked]').attr('data-checked', false)
+    let dataSliderId = $(this).parent().parent().parent().parent().parent().find('[data-slider-id]').attr('data-slider-id')[0]
+    $(this).parent().parent().find('[data-checked-all]').attr('data-checked-all', true)
+    $(this).parent().parent().find('[data-click="category"]').removeClass('section-detail-list__item--active')
+    $(this).parent().parent().find('[data-click="category"]').removeClass('colored_theme_bg')
+    $(this).toggleClass('section-detail-list__item--active')
+    $(this).toggleClass('colored_theme_bg')
+    updateProductList(dataSliderId, dataStoneList[dataSliderId], stonesItemList[dataSliderId])
+    checkedList[dataSliderId] = []
+  }
 })
